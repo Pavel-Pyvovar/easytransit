9288e4ef9a4cbe19b3e233d09613d9add67ea342
Pavel Pyvovar <jalovanfres@gmail.com>
2019-12-23 17:40:38 +0200

    Issue with login solved

--- app_pack/forms.py
+++ app_pack/forms.py
@@ -4,9 +4,9 @@ from wtforms.validators import DataRequired, InputRequired, Email
 
 class LoginForm(FlaskForm):
     # username = StringField('Username', validators=[DataRequired()])
-    # first_name = StringField('First name', validators=[DataRequired()])
-    # second_name = StringField('Second name', validators=[DataRequired()])
 
+    first_name = StringField('First name', validators=[DataRequired()])
+    second_name = StringField('Second name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
